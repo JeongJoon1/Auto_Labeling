f28602b2bcb3bb830070b06d97eff2f8aaff4f5d
dobyming <12184874@inha.edu>
2022-04-05 18:44:46 +0900

    Scroll_down , 디렉토리 생성. 이미지 다운로드(일부 사진이 제대로 다운 안되는 오류 발생)

--- Auto_Labeling/spiders/imgcrawler.py
+++ Auto_Labeling/spiders/imgcrawler.py
@@ -2,8 +2,12 @@ import scrapy
 from selenium import webdriver
 from urllib.parse import quote_plus
 from urllib.request import urlopen
+import urllib.request
+import time
+import os
 
 class ImgcrawlerSpider(scrapy.Spider):
+    #Base_URL Setting
     name = 'imgcrawler'
     allowed_domains = ['www.naver.com']
     # start_urls = ['http://www.naver.com']
@@ -13,16 +17,53 @@ class ImgcrawlerSpider(scrapy.Spider):
     search_url = base+quote_plus(keyword)
     driver = webdriver.Chrome()
     driver.get(search_url)
+    
+    #scroll_down 구현부
+    SCROLL_PAUSE_TIME = 1
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(SCROLL_PAUSE_TIME)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height: 
+            break
+        last_height = new_height
 
-    custom_settings = {
-        'DOWNLOADER_MIDDLEWARES': { 
-            'Auto_Labeling.middlewares.AutoLabelingDownloaderMiddleware': 100 
-        }
-    }
+    #디렉토리 생성
+    def create_folder(directory):
+        try:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+        except OSError:
+            print("Error : Creating Directory. "+directory)
+    
+    save_path = './img/'+keyword+"/"
+    crawl_file = create_folder(save_path)
+
+    #이미지를 긁어오기(Crawling)
+    images = driver.find_elements_by_class_name("_image")
+    cnt=1
+    for image in images:
+        try:
+            imgUrl= image.get_attribute('src')
+            with urllib.request.urlopen(imgUrl) as f:
+                with open(save_path + keyword + str(cnt) + '.jpg', 'wb') as h:
+                    img = f.read()
+                    h.write(img)
+            cnt+=1
+        except:
+            pass    
+        
+    driver.close()
+    #이미지 다운로드 
+
+    # custom_settings = {
+    #     'DOWNLOADER_MIDDLEWARES': { 
+    #         'Auto_Labeling.middlewares.AutoLabelingDownloaderMiddleware': 100 
+    #     }
+    # }
 
     # def __init__(self, *args, **kargs):
-        
-        
 
     def start_requests(self):
         for url in self.start_urls:
