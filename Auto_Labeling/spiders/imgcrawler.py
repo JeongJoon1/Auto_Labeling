8d9ae28bd684bb6a0aef4df528a54b3df7781163
dobyming <12184874@inha.edu>
2022-04-10 13:33:55 +0900

    naver crawler (일부 이미지에 대해서 사진이 안보이는 issue)

--- Auto_Labeling/spiders/imgcrawler.py
+++ Auto_Labeling/spiders/imgcrawler.py
@@ -19,7 +19,7 @@ class ImgcrawlerSpider(scrapy.Spider):
     driver.get(search_url)
     
     #scroll_down 구현부
-    SCROLL_PAUSE_TIME = 1
+    SCROLL_PAUSE_TIME = 3 #고민부분 
     last_height = driver.execute_script("return document.body.scrollHeight")
     while True:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -55,31 +55,7 @@ class ImgcrawlerSpider(scrapy.Spider):
             pass    
         
     driver.close()
-    #이미지 다운로드 
-
-    # custom_settings = {
-    #     'DOWNLOADER_MIDDLEWARES': { 
-    #         'Auto_Labeling.middlewares.AutoLabelingDownloaderMiddleware': 100 
-    #     }
-    # }
-
-    # def __init__(self, *args, **kargs):
 
     def start_requests(self):
         for url in self.start_urls:
             yield scrapy.Request(url=url, callback=self.parse, method='GET', encoding='utf-8')
-
-
-    def parse(self, response):
-        contents = response.xpath('//*[@id="main_pack"]/section[2]/div')
-        for content in contents:
-            small_img = content.xpath('div[1]/div[1]/div[1]/div/div[1]/a/img').extract_first()
-            big_img = content.xpath('div[2]/div/div/div[1]/div[1]/div/div/div[1]/div[1]/img').extract_first()
-            item = {
-                'small_img' : small_img.strip() if small_img else small_img
-                ,'big_img' : big_img.strip() if big_img else big_img
-            }
-            print(item)
-
-            yield item 
-        #  print(response.url, response.body)
