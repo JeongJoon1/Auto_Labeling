75d94f53ea84b2dfc08b1dcae9ae5482011af69a
dobyming <12184874@inha.edu>
2022-06-01 02:07:43 +0900

    Amazon S3 데이터공유 탑재

--- GUI/main_GUI.py
+++ GUI/main_GUI.py
@@ -2,6 +2,7 @@ from msilib.schema import Font
 from tkinter import *
 import tkinter.font as tkfont
 from tkinter import filedialog
+from turtle import width
 import scrapy
 from scrapy.crawler import CrawlerProcess 
 from scrapy.utils.project import get_project_settings
@@ -14,10 +15,15 @@ import urllib.request
 import sys
 import time
 import os
+from urllib import response
+import boto3
+import os
+from botocore.client import Config
+from requests import session
 
 main = Tk()
 main.title('Image Auto Labeling Tool')
-main.geometry('800x500')
+main.geometry('620x500')
 main.resizable(False, False)
 
 #크롤링 창  
@@ -25,7 +31,8 @@ def createCrawlpage():
     Crawlpage = Toplevel(main)
     Crawlpage.geometry('700x300') 
     Crawlpage.resizable(False, False)
-    
+    Crawlpage.title("Image Crawling")
+
     ctitlesize = tkfont.Font(size=20)
     cpsize = tkfont.Font(size=11)
     crawl_label1 = Label(Crawlpage,text="크롤링", height="3",font=ctitlesize)   
@@ -34,7 +41,8 @@ def createCrawlpage():
     search = StringVar() #검색어변수 = search 
     ctextbox = Entry(Crawlpage,width=20,textvariable=search) #검색어 입력부 
     ctextbox.place(x=270, y=150)
-
+   
+    #구글 크롤링
     def Gtest():
         #Chrome(spider #1)
         class GcrawlerSpider(scrapy.Spider):
@@ -94,6 +102,8 @@ def createCrawlpage():
                 except:
                     pass   
             driver.close()
+
+    #네이버 크롤링 
     def Ntest():
         #Naver(spider #2)
         class ImgcrawlerSpider(scrapy.Spider):
@@ -145,9 +155,7 @@ def createCrawlpage():
                 except:
                     pass    
             driver.close()
-
-    search_btn = Button(Crawlpage,text="검색",command="") #검색 클릭 시 크롤링 시작 
-    search_btn.place(x=420, y=147)
+  
 
     Gsearch_btn = Button(Crawlpage,text="구글",command=Gtest) #구글 검색 버튼
     Gsearch_btn.place(x=230, y=193)
@@ -158,6 +166,7 @@ def createCrawlpage():
     Pixaby_btn = Button(Crawlpage,text="Pixaby",command="") #Pixaby 검색 버튼(미완성)
     Pixaby_btn.place(x=410, y=193)
 
+    #폴더열기
     folder_btn = Button(Crawlpage,text="폴더열기",command=folder_open)
     folder_btn.place(x=312, y=253)
 
@@ -167,15 +176,73 @@ def createCrawlpage():
 def folder_open():
     filedialog.askopenfilename(initialdir='./img',title='파일선택', filetypes=(('jpg files','*.jpg'),('all files','*.*')))
 
+
+
+#데이터셋 공유 창
+def createSharepage():
+    Sharepage = Toplevel(main)
+    dirpath = Entry(Sharepage,width=100) #파일경로 불러옴 
+    dirpath.pack(fill="x",padx=1,pady=1)
+
+    #파일 경로를 불러옴
+    def file_find():
+        Sharepage.dirName=filedialog.askdirectory()
+        dirpath.delete(0,END)
+        dirpath.insert(END,Sharepage.dirName)
+
+    #AWS 권한 명시 
+    session=boto3.Session(
+        aws_access_key_id='AKIAXZWL4PM4KT75FEOG',
+        aws_secret_access_key='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',
+        region_name='ap-northeast-2'
+        )
+    s3 = session.client('s3')
+
+    #업로드 할 s3 버킷명
+    bucket='autolabeling'
+
+    def upload_dir():
+        local_dir=dirpath.get()
+        # enumerate local files recursively
+        for root, dirs, files in os.walk(local_dir):
+            for filename in files:
+                # construct the full local path
+                local_path = os.path.join(root, filename)
+
+                # construct the full Dropbox path
+                relative_path = os.path.relpath(local_path, local_dir)
+                s3_path = os.path.join(root, relative_path)
+
+                print ('Searching "%s" in "%s"' % (s3_path, bucket))
+                try:
+                    s3.head_object(Bucket=bucket, Key=s3_path)
+                    print ("Path found on S3! Skipping %s..." % s3_path)
+                except:
+                    print ("Uploading %s..." % s3_path)
+                    s3.upload_file(local_path, bucket, s3_path)
+
+    fr_bt = Frame(Sharepage)
+    fr_bt.pack(fill="x",padx=1,pady=1)
+
+    bt_upload = Button(fr_bt,text="Upload",width=10,command=upload_dir) #업로드 버튼
+    bt_upload.pack(side="right",padx=1,pady=1)
+    bt_find = Button(fr_bt,text="Find",width=10,command=file_find) #dir 불러오는 버튼
+    bt_find.pack(side="right",padx=1,pady=1)
+
+    Sharepage.title("Dataset Share") #창 title
+
+
 #초기화면 제목부 
 fontsize = tkfont.Font(size=20)
 title_label = Label(main,text="Image Auto Labeling Tool", height="3",font=fontsize)
 
 #초기화면 버튼부 
-crawl_btn = Button(main, text="크롤링", width="20", height="5",command=createCrawlpage)
-crawl_btn.place(x=150, y=100)
-label_btn = Button(main, text="자동라벨링", width="20", height="5")
-label_btn.place(x=450, y=100)
+crawl_btn = Button(main, text="크롤링", width="17", height="5",command=createCrawlpage)
+crawl_btn.place(x=80, y=200)
+label_btn = Button(main, text="자동라벨링", width="17", height="5")
+label_btn.place(x=260, y=200)
+share_btn=Button(main,text="데이터셋 공유",width="17",height="5",command=createSharepage)
+share_btn.place(x=450,y=200)
 
 title_label.pack()
 main.mainloop() 
